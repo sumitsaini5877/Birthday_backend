fcd011f3a6a8ce25dcc201a47ce5a00e35b32762
Sumit Saini <sumitsaini5877@gmail.com>
2026-07-13 23:21:47 +0530

    solve corn issue

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app.include_router(router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://birthdaygift-gamma-two.vercel.app/","http://localhost:5500","http://127.0.0.1:5500"],  
+    allow_origins=["https://birthdaygift-gamma-two.vercel.app","http://localhost:5500","http://127.0.0.1:5500"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
